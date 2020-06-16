7e391cf26b3f6929837d0069da425f9dc6c09901
IsaiasMorochi <isaiasmorochi95@gmail.com>
2020-06-15 21:46:18 -0400

    Configuracion sockt

--- classes/server.ts
+++ classes/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { environment } from '../environments/environment'
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
 
@@ -7,14 +9,32 @@ export default class Server {
     public hostname: string;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.hostname = environment.HOSTNAME
         this.port = environment.SERVER_PORT;
+
+        // socket necesita recibir la conf del servidor (Http)
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO( this.httpServer );
+
+        this.listenSockets();
+
     }
 
     start( callback: VoidFunction ) {
-        this.app.listen( this.port, this.hostname, callback);
+        this.httpServer.listen( this.port, this.hostname, callback);
+    }
+
+    private listenSockets() {
+        console.log('***** Start Escuchar conexiones - socket *****');
+        this.io.on('connection', client => {
+            console.log('Cliente conectado.');
+        })
+        console.log('***** End Escuchar conexiones - socket *****');
     }
 
 }
